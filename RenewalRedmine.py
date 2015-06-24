8902259afa96ab4621620b7a9076036a8b60503b
David Edgerton, Jr <davidedgertonjr@gmail.com>
2015-06-24 14:10:34 -0500

    Create xlsx.file with code

--- RenewalRedmine.py
+++ RenewalRedmine.py
@@ -1,89 +1,98 @@
 
 from redmine import Redmine
+import settings
+import xlsxwriter
 
-redmine = Redmine('http://renewal.plan.io',
-                  username='A62478',
-                  password='Django2015!?',
+redmine = Redmine(settings.SITE,
+                  username=settings.USERNAME,
+                  password=settings.PASSWORD,
                   requests={'verify':False}
                   )
 
-project = redmine.project.get('baan')
-
 # Open file for writing
-
-f = open('/Users/davidedgertonjr/Desktop/data.txt','w',encoding='utf-8')
-
-# Create the header row for the CSV file
-
-header = 'ID|SUBJECT|DESCRIPTION|AUTHOR|CREATED_ON|ASSIGNED_TO|ESTIMATED_HOURS|PRIORITY|START_DATE|STATUS|TRACKER|UPDATED_ON'
-
+    
+f = open('c:/users/a62478/Desktop/data.txt','w',encoding='utf-8')
+    
+# Create the header row for the text file
+    
+header = 'ISSUE_ID|SUBJECT|DESCRIPTION|AUTHOR|CREATED_ON|ASSIGNED_TO|ESTIMATED_HOURS|PRIORITY|START_DATE|STATUS|TRACKER|UPDATED_ON'
+    
 f.write(header)
 f.write('\n')
 
-for issue in project.issues:
+#Add all project information to the text file
 
-# if the object has the attribute print it
+RedmineProjects = ['baan','mes','pcf']
 
-    if hasattr(issue, 'id'):
-        line = str(issue.id)
-    else:
-        line = 'NA'
+for project in RedmineProjects:
 
-    if hasattr(issue, 'subject'):
-        line = line + '|' + str(issue.subject)
-    else:
-        line = line + '|' + 'NA'
+    selected_project = redmine.project.get(project)
 
-    if hasattr(issue, 'description'):
-        line = line + '|' + str(issue.description).replace('\n', ' ').replace('\r', '')
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'author'):
-        line = line + '|' + str(issue.author)
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'created_on'):
-        line = line + '|' + str(issue.created_on)
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'assigned_to'):
-        line = line + '|' + str(issue.assigned_to)
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'estimated_hours'):
-        line = line + '|' + str(issue.estimated_hours)
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'priority'):
-        line = line + '|' + str(issue.priority)
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'start_date'):
-        line = line + '|' + str(issue.start_date)
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'status'):
-        line = line + '|' + str(issue.status)
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'tracker'):
-        line = line + '|' + str(issue.tracker)
-    else:
-        line = line + '|' + 'NA'
-
-    if hasattr(issue, 'updated_on'):
-        line = line + '|' + str(issue.updated_on)
-    else:
-        line = line + '|' + 'NA'
-
-    f.write(line + '\n')
-
+    for issue in selected_project.issues:
+    
+    # if the object has the attribute print it
+    
+        if hasattr(issue, 'id'):
+            line = str(issue.id)
+        else:
+            line = 'NA'
+    
+        if hasattr(issue, 'subject'):
+            line = line + '|' + str(issue.subject)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'description'):
+            line = line + '|' + str(issue.description).replace('\n', ' ').replace('\r', '')
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'author'):
+            line = line + '|' + str(issue.author)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'created_on'):
+            line = line + '|' + str(issue.created_on)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'assigned_to'):
+            line = line + '|' + str(issue.assigned_to)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'estimated_hours'):
+            line = line + '|' + str(issue.estimated_hours)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'priority'):
+            line = line + '|' + str(issue.priority)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'start_date'):
+            line = line + '|' + str(issue.start_date)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'status'):
+            line = line + '|' + str(issue.status)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'tracker'):
+            line = line + '|' + str(issue.tracker)
+        else:
+            line = line + '|' + 'NA'
+    
+        if hasattr(issue, 'updated_on'):
+            line = line + '|' + str(issue.updated_on)
+        else:
+            line = line + '|' + 'NA'
+    
+        f.write(line + '\n')
+    
 f.close()
+
---
+++ settings.py
@@ -0,0 +1,4 @@
+SITE = 'http://renewal.plan.io'
+USERNAME = 'A62478'
+PASSWORD = 'Django2015!?'
+REQUESTS = "{'verify':False}"
